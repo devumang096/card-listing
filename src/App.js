c5e16b2702bf324eb1454a3b05257a26e4b64244
Umang Devanshu <devumang096@gmail.com>
2023-06-17 12:27:58 +0530

    adding css

--- src/App.js
+++ src/App.js
@@ -2,46 +2,147 @@ import React, { useEffect, useState } from 'react';
 import CardList from './components/CardList';
 import './App.css';
 
+
 const jsonData = {
-  data: [
+  "data": [
     {
-      name: 'Mixmax',
-      budget_name: 'Software subscription',
-      owner_id: 1,
-      spent: {
-        value: 100,
-        currency: 'SGD',
+      "name": "Mixmax",
+      "budget_name": "Software subscription",
+      "owner_id": 1,
+      "spent": {
+        "value": 100,
+        "currency": "SGD"
       },
-      available_to_spend: {
-        value: 1000,
-        currency: 'SGD',
+      "available_to_spend": {
+        "value": 1000,
+        "currency": "SGD"
       },
-      card_type: 'burner',
-      expiry: '9 Feb',
-      limit: 100,
-      status: 'active',
+      "card_type": "burner",
+      "expiry": "9 Feb",
+      "limit": 100,
+      "status": "active"
     },
     {
-      name: 'Quickbooks',
-      budget_name: 'Software subscription',
-      owner_id: 2,
-      spent: {
-        value: 50,
-        currency: 'SGD',
+      "name": "Quickbooks",
+      "budget_name": "Software subscription",
+      "owner_id": 2,
+      "spent": {
+        "value": 50,
+        "currency": "SGD"
       },
-      available_to_spend: {
-        value: 250,
-        currency: 'SGD',
+      "available_to_spend": {
+        "value": 250,
+        "currency": "SGD"
       },
-      card_type: 'subscription',
-      limit: 10,
-      status: 'active',
+      "card_type": "subscription",
+      "limit": 10,
+      "status": "active"
     },
+    {
+      "name": "Card 3",
+      "budget_name": "Budget 3",
+      "owner_id": 1,
+      "spent": {
+        "value": 200,
+        "currency": "SGD"
+      },
+      "available_to_spend": {
+        "value": 800,
+        "currency": "SGD"
+      },
+      "card_type": "burner",
+      "expiry": "15 Mar",
+      "limit": 150,
+      "status": "active"
+    },
+    {
+      "name": "Card 4",
+      "budget_name": "Budget 4",
+      "owner_id": 2,
+      "spent": {
+        "value": 75,
+        "currency": "SGD"
+      },
+      "available_to_spend": {
+        "value": 225,
+        "currency": "SGD"
+      },
+      "card_type": "subscription",
+      "limit": 20,
+      "status": "blocked"
+    },
+    {
+      "name": "Card 5",
+      "budget_name": "Budget 5",
+      "owner_id": 1,
+      "spent": {
+        "value": 300,
+        "currency": "SGD"
+      },
+      "available_to_spend": {
+        "value": 700,
+        "currency": "SGD"
+      },
+      "card_type": "burner",
+      "expiry": "25 Apr",
+      "limit": 200,
+      "status": "active"
+    },
+    {
+      "name": "Card 6",
+      "budget_name": "Budget 6",
+      "owner_id": 2,
+      "spent": {
+        "value": 120,
+        "currency": "SGD"
+      },
+      "available_to_spend": {
+        "value": 130,
+        "currency": "SGD"
+      },
+      "card_type": "subscription",
+      "limit": 5,
+      "status": "active"
+    },
+    {
+      "name": "Card 7",
+      "budget_name": "Budget 7",
+      "owner_id": 1,
+      "spent": {
+        "value": 50,
+        "currency": "SGD"
+      },
+      "available_to_spend": {
+        "value": 450,
+        "currency": "SGD"
+      },
+      "card_type": "burner",
+      "expiry": "10 May",
+      "limit": 300,
+      "status": "blocked"
+    },
+    {
+      "name": "Card 8",
+      "budget_name": "Budget 8",
+      "owner_id": 2,
+      "spent": {
+        "value": 80,
+        "currency": "SGD"
+      },
+      "available_to_spend": {
+        "value": 170,
+        "currency": "SGD"
+      },
+      "card_type": "subscription",
+      "limit": 15,
+      "status": "active"
+    }
   ],
-  page: 1,
-  per_page: 10,
-  total: 100,
+  "page": 1,
+  "per_page": 10,
+  "total": 100
 };
+
 
 const App = () => {
   const [activeTab, setActiveTab] = useState('Your');
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from './Card';
 import './CardList.css';
 
 const CardList = ({ cards }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filterType, setFilterType] = useState('');
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleFilterChange = (event) => {
+    setFilterType(event.target.value);
+  };
+
+  const filteredCards = cards.filter((card) => {
+    const nameMatch = card.name.toLowerCase().includes(searchTerm.toLowerCase());
+    const typeMatch = filterType === '' || card.card_type === filterType;
+    return nameMatch && typeMatch;
+  });
+
   return (
     <div className="card-list">
-      {cards.map((card, index) => (
+      <div className="filter-section">
+        <input
+          type="text"
+          placeholder="Search by card name"
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+        <select value={filterType} onChange={handleFilterChange}>
+          <option value="">All</option>
+          <option value="burner">Burner</option>
+          <option value="subscription">Subscription</option>
+        </select>
+      </div>
+      {filteredCards.map((card, index) => (
         <Card card={card} key={index} />
       ))}
     </div>
